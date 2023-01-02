74b368abd6c22da0d081ed1f4243e4bf2e767b48
ZYK <14935309+zyk0615@users.noreply.github.com>
2023-01-02 21:38:42 +0800

    add check sn uni in data count

--- src/DPFactory/Factory/AbstractUniSn.php
+++ src/DPFactory/Factory/AbstractUniSn.php
@@ -4,6 +4,7 @@ namespace ZYKUtil\UniSn\DPFactory\Factory;
 
 use ZYKUtil\UniSn\IUniSn;
 use ZYKUtil\UniSn\ResolverTrait;
+use Illuminate\Support\Facades\DB;
 
 abstract class AbstractUniSn implements IUniSn
 {
@@ -14,4 +15,28 @@ abstract class AbstractUniSn implements IUniSn
      * @return string
      */
     abstract public function generate(): string;
+
+    protected function checkDataCount($prefix , &$count = 1)
+    {
+        $sn = $prefix . str_pad(
+                $count,
+                $this->resolver->getArgs()['default']['pad_length'],
+                0,
+                STR_PAD_LEFT
+            );
+        $status = DB::table($this->resolver->getArgs()['default']['table'])->where(
+                $this->resolver->getArgs()['default']['column'], $sn
+            )->count($this->resolver->getArgs()['default']['column']) > 0;
+
+        if ($status) {
+            $count++;
+            self::checkDataCount($prefix , $count);
+        }
+        return $prefix . str_pad(
+                $count,
+                $this->resolver->getArgs()['default']['pad_length'],
+                0,
+                STR_PAD_LEFT
+            );
+    }
 }
